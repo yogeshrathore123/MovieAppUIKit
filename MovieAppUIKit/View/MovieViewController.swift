267c182f16ad719f2c24cee1e405a4f51af807ae
Yogesh Rathore <yogeshrathore251@gmail.com>
2025-06-17 14:45:46 +0530

    Initial commit

--- MovieAppUIKit/View/MovieViewController.swift
+++ MovieAppUIKit/View/MovieViewController.swift
@@ -1,29 +1,99 @@
 //
-//  MovieViewCOntroller.swift
+//  MovieViewController.swift
 //  MovieAppUIKit
 //
 //  Created by Yogesh Rathore on 17/06/25.
 //
 
 import UIKit
+import SwiftUI
 
-class MovieViewCOntroller: UIViewController {
-
+class MovieViewController: UIViewController {
+    
+    lazy var searchBar: UISearchBar = {
+        let searchBar = UISearchBar()
+        searchBar.translatesAutoresizingMaskIntoConstraints = false
+        searchBar.placeholder = "Search"
+        searchBar.delegate = self
+        return searchBar
+    }()
+    
+    lazy var movieTableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.delegate = self
+        tableView.dataSource = self
+        return tableView
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        setUpUI()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setUpUI() {
+        view.backgroundColor = .white
+        
+        // register cells
+        movieTableView.register(UITableViewCell.self, forCellReuseIdentifier: "MovieTableViewCell")
+        
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(stackView)
+        
+        stackView.topAnchor.constraint(equalTo: view.topAnchor, constant: 60).isActive = true
+        stackView.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+        stackView.heightAnchor.constraint(equalTo: view.heightAnchor).isActive = true
+        stackView.addArrangedSubview(searchBar)
+        stackView.addArrangedSubview(movieTableView)
+        
+        searchBar.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        searchBar.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        searchBar.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
     }
-    */
+    
+}
 
+extension MovieViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieTableViewCell", for: indexPath)
+        var content = cell.defaultContentConfiguration()
+        content.text = "Hello SwiftUI"
+        cell.contentConfiguration = content
+        return cell
+    }
+    
+    
+}
+
+extension MovieViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        print("SearchBar: \(searchText)")
+    }
+}
+
+struct MovieViewControllerRepresentable: UIViewControllerRepresentable {
+    
+    typealias UIViewControllerType = MovieViewController
+    
+    func makeUIViewController(context: Context) -> MovieViewController {
+        MovieViewController()
+    }
+    
+    func updateUIViewController(_ uiViewController: MovieViewController, context: Context) {
+        
+    }
+}
+
+#Preview {
+    MovieViewControllerRepresentable()
 }
